53f955298950c6f4bd4bd67f7ace48d81ce472e1
DaanSleven <daansleven@hotmail.com>
2024-09-07 16:43:49 +0200

    Added a connection to firebase's realtime database. Firebase needs a secret to their database which we allow users to store in their cookies. Added some tests on the lobby page for accessing and updating the firebase database.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref } from 'firebase/database';
+import Cookies from 'js-cookie';
+import cookieNames from './enums/cookieNames';
+
+export const firebaseApp = initializeApp({
+	apiKey: Cookies.get(cookieNames.FirebaseSecret),
+	databaseURL: "https://orcs-must-read-unchained-default-rtdb.europe-west1.firebasedatabase.app/"
+});
+
+export const database = getDatabase(firebaseApp);
--- src/pages/LobbyPage.vue
+++ src/pages/LobbyPage.vue
@@ -1,5 +1,10 @@
 <template>
-	Coming Soon!
+	<v-text-field label="Firebase secret" variant="outlined" hide-details v-model="firebaseSecret" type="password"/>
+	<v-btn color="primary" @click="setFirebaseSecret">
+		Apply
+	</v-btn>
+
+	<h2 class="mt-8">Some testing</h2>
 	<div @click="setPlayerLoadout(1, 'Daan-C0n1-2L2H1e2B1g1k3P3G2P-BI-00-000c-H00M00R00A00M00500000000C00')">
 		Adjust 1 Daan
 	</div>
@@ -18,10 +23,14 @@
 <script>
 import { database } from '../firebase.js';
 import { set, ref } from 'firebase/database';
+import Cookies from 'js-cookie';
+import cookieNames from '../enums/cookieNames.js';
 
 export default {
 	data() {
 		return {
+			firebaseSecret: Cookies.get(cookieNames.FirebaseSecret) ?? null,
+
 			playerLoadouts: []
 		}
 	},
@@ -31,15 +40,17 @@ export default {
 	computed: {
 		player1Loadout() {
 			return this.playerLoadouts.find((loadout) => loadout.id === "player-1")
-		},
-		player1Loadout() {
-			return this.playerLoadouts.find((loadout) => loadout.id === "player-1")
 		}
 	},
 	methods: {
+		setFirebaseSecret() {
+			// Set the cookie for 10 years
+			Cookies.set(cookieNames.FirebaseSecret, this.firebaseSecret, 365);
+			location.reload();
+		},
 		setPlayerLoadout(playerNumber, loadoutString) {
 			set(ref(database, `player-loadouts/player-${playerNumber}`), loadoutString);
-		}
+		},
 	},
 };
 </script>
