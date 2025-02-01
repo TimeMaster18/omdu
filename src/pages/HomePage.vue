56891dba10216ff4584dca80f2851cdc38be0a8b
DaanSleven <daansleven@hotmail.com>
2025-02-01 15:48:28 +0100

    Added a little welcome text to the home page explaining the project and linking users back to Project Unchained.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,30 +1,54 @@
 <template>
-    <div>
-        <v-card
-            max-width="700"
-            class="mx-auto text-center"
+    <div class="d-flex justify-center">
+        <img
+            class="logo"
+            src="../assets/images/logo.png"
         >
-            <v-card-title>
-                Fair Use / Fair Dealing Disclaimer
-            </v-card-title>
-            <v-card-text>
-                <img
-                    class="logo"
-                    src="../assets/images/logo.png"
-                >
-                
-                <p class="mb-4">
-                    This website is a fan-made project dedicated to Orcs Must Die! Unchained, a discontinued game originally developed and published by <a href="https://robotentertainment.com/">Robot Entertainment</a>. The images, logos, and other copyrighted materials used on this site are the property of their respective owners.
-                </p>
-                <p class="mb-4">
-                    The use of these materials is done in accordance with applicable EU copyright laws, including exceptions for non-commercial, educational, and archival purposes as outlined in the EU Copyright Directive (Directive 2019/790) and relevant national laws. This website is not affiliated with, endorsed by, or sponsored by Robot Entertainment. No copyright infringement is intended.
-                </p>
-                <p>
-                    If you are a rights holder and believe any content should be removed or modified, please contact me at <a href="mailto:tripledutch19@gmail.com">tripledutch19@gmail.com</a>, and I will address your concerns promptly.
-                </p>
-            </v-card-text>
-        </v-card>
     </div>
+    <v-row class="justify-center">
+        <v-col cols="auto">
+            <v-card
+                max-width="700"
+                class="mx-auto text-center"
+            >
+                <v-card-title>
+                    Welcome to Orcs Must Read Unchained!
+                </v-card-title>
+                <v-card-text>
+                    <p class="mb-4">
+                        I created this small website to help my friends and I set up survival mode loadouts for <a href="https://github.com/TimeMaster18/Project-Rechained">Project Rechained</a>, a fan-made revival of Orcs Must Die! What began as a personal project ended up being useful to more people than I anticipated, so I'm now working on expanding the site for a broader audience. I hope it helps you get more familiar with the game and enhances your experience.
+                    </p>
+                    <p class="mb-4">
+                        To get started playing the game, follow the installation instructions on <a href="https://github.com/TimeMaster18/Project-Rechained">Project Rechained's GitHub page</a>.
+                    </p>
+                    <p>
+                        Looking for a group to play with? Join <a href="https://discord.gg/xkZskPXtwm">Project Unchained's Discord server</a>, where you'll find dedicated LFG channels and roles.
+                    </p>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="auto">
+            <v-card
+                max-width="700"
+                class="mx-auto text-center"
+            >
+                <v-card-title>
+                    Fair Use / Fair Dealing Disclaimer
+                </v-card-title>
+                <v-card-text>                
+                    <p class="mb-4">
+                        This website is a fan-made project dedicated to Orcs Must Die! Unchained, a discontinued game originally developed and published by <a href="https://robotentertainment.com/">Robot Entertainment</a>. The images, logos, and other copyrighted materials used on this site are the property of their respective owners.
+                    </p>
+                    <p class="mb-4">
+                        The use of these materials is done in accordance with applicable EU copyright laws, including exceptions for non-commercial, educational, and archival purposes as outlined in the EU Copyright Directive (Directive 2019/790) and relevant national laws. This website is not affiliated with, endorsed by, or sponsored by Robot Entertainment. No copyright infringement is intended.
+                    </p>
+                    <p>
+                        If you are a rights holder and believe any content should be removed or modified, please contact me at <a href="mailto:tripledutch19@gmail.com">tripledutch19@gmail.com</a>, and I will address your concerns promptly.
+                    </p>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -33,6 +57,7 @@ export default {}
 
 <style scoped>
 .logo {
-    width: 75%;
+    width: 100%;
+    max-width: 500px;
 }
 </style>
