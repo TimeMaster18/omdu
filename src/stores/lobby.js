8e03254ba10dc034b0f729210ba283ea9cfc3404
DaanSleven <daansleven@hotmail.com>
2025-02-04 21:19:32 +0100

    Fixed an issue with secure pages making unsecure socket connections.

--- src/stores/lobby.js
+++ src/stores/lobby.js
@@ -16,8 +16,9 @@ export const useLobbyStore = defineStore('lobby', {
         connect(ip) {
             // Prevent a double connection
             if(this.socket !== null) return;
-
-            this.socket = new WebSocket(`ws://${ip}:7778/lobby`);
+            
+            const prefix = window.location.protocol === 'https:' ? 'wss' : 'ws';
+            this.socket = new WebSocket(`${prefix}://${ip}:7778/lobby`);
             this.socket.onopen = () => {
                 this.socket.onmessage = (message) => {
                     const data = JSON.parse(message.data);
